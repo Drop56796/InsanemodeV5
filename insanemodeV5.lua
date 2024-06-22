1d58b6046cd1534cd27566b01c33cbcb5b22894e
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-22 12:41:49 +0800

    Update insanemodeV5.lua

--- insanemodeV5.lua
+++ insanemodeV5.lua
@@ -14,22 +14,12 @@ coroutine.wrap(function()
         wait(math.random(250,460))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         wait(0.3)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Mayhem-mode-enity/main/Manic%20Rush.lua"))()
-    end
-end)()
-
--- Threat
-coroutine.wrap(function()
-    while true do
-        wait(math.random(350,450))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(0.3)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoangnguyen72628/MayhemMode/main/ObfuscatedEntities/Threat-obfuscated.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/InsanemodeV5/main/slience.lua"))()
     end
 end)()
 
 -- Set initial properties for MainColorCorrection to make the scene darker
-game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(128, 128, 128) -- Darker tint
+game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(128, 128, 128)
 game.Lighting.MainColorCorrection.Contrast = 0.5
 game.Lighting.MainColorCorrection.Saturation = -0.35
 
