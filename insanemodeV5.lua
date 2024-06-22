7d584142e64fbba78b299a9d10ccaf0db900b151
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-22 12:52:18 +0800

    Update insanemodeV5.lua

--- insanemodeV5.lua
+++ insanemodeV5.lua
@@ -11,7 +11,7 @@ end)()
 -- Manci Rush
 coroutine.wrap(function()
     while true do
-        wait(math.random(250,460))
+        wait(150)
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         wait(0.3)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/InsanemodeV5/main/slience.lua"))()
@@ -22,6 +22,7 @@ end)()
 game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(128, 128, 128)
 game.Lighting.MainColorCorrection.Contrast = 0.5
 game.Lighting.MainColorCorrection.Saturation = -0.35
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/InsanemodeV5/main/Sprint.lua"))()
 
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Insane mode Initiated",true)
 game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
@@ -33,3 +34,4 @@ wait(3)
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("For the sake of the game experience, please do not use destructive scripts",true)
 wait(3)
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Start playing. Good luck!",true)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Jahani-john/mayhem-mode/main/mayhemmode-main/loader.lua'))()
